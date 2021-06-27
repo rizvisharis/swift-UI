606bad2030ab1f0c889d6464372d1339e1d596ae
Rizvi <rizvisharis@gmail.com>
2021-06-27 19:35:32 +0530

    Initial Commit

---
+++ sas_kitchen/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  sas_kitchen
+//
+//  Created by Rizvi Sharis on 2021-06-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ sas_kitchen/sas_kitchenApp.swift
@@ -0,0 +1,17 @@
+//
+//  sas_kitchenApp.swift
+//  sas_kitchen
+//
+//  Created by Rizvi Sharis on 2021-06-27.
+//
+
+import SwiftUI
+
+@main
+struct sas_kitchenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
